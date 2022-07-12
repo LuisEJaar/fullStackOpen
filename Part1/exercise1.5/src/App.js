861541371fee1560e4a1fb3738f45db4cc29538d
LuisEJaar <luisjaarcoding@gmail.com>
2022-07-12 15:12:28 -0500

    Completed exercise1.5

---
+++ Part1/exercise1.5/src/App.js
@@ -0,0 +1,67 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }]
+  }
+   
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+      <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        <Part part={props.parts[0].name} exercises = {props.parts[0].exercises}/>
+      </p>
+      <p>
+        <Part part={props.parts[1].name} exercises = {props.parts[1].exercises}/>
+      </p>
+      <p>
+        <Part part={props.parts[2].name} exercises = {props.parts[2].exercises}/>
+      </p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <>
+    {props.part} {props.exercises}
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+export default App
